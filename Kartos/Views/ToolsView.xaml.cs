266091ba0b83d0bd16cf91d8aa93f7c121728e53
LAPTOP-IJPP57VS\user <jaycejacquez9494@gmail.com>
2024-04-15 10:27:46 +0800

    add polygon

--- Kartos/Views/ToolsView.xaml.cs
+++ Kartos/Views/ToolsView.xaml.cs
@@ -38,6 +38,7 @@ namespace Kartos.Views
         Thickness _mouseDownMargin;
         private Image _currentImage;
         private Canvas _overlayCanvas;
+        private Mat mat;
 
 
         public ToolsView()
@@ -45,12 +46,12 @@ namespace Kartos.Views
             InitializeComponent();
             toolsViewModel = new ToolsViewModel();
             this.DataContext = toolsViewModel;
-            
+
         }
 
         private void Image_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            if (_isMouseDown == true && isDrawingLine == false)
+            if (_isMouseDown == true && isDrawingLine == false && isDrawingPolygon == false)
             {
                 var c = sender as Image;
                 var pos = e.GetPosition(this);
@@ -117,7 +118,7 @@ namespace Kartos.Views
                 string selectedFileName = openFileDialog.FileName;
 
                 // Read the image using OpenCVSharp
-                Mat mat = Cv2.ImRead(selectedFileName);
+                mat = Cv2.ImRead(selectedFileName);
 
                 Image img = new Image()
                 {
@@ -125,6 +126,7 @@ namespace Kartos.Views
                     Height = 500,
                     Source = mat.ToBitmapSource()
                 };
+                _currentImage = img;
                 img.AddHandler(Button.MouseDownEvent, new MouseButtonEventHandler(Image_PreviewMouseDown), true);
                 img.AddHandler(Button.MouseMoveEvent, new MouseEventHandler(Image_PreviewMouseMove), true);
                 img.AddHandler(Button.MouseUpEvent, new MouseButtonEventHandler(Image_PreviewMouseUp), true);
@@ -133,6 +135,8 @@ namespace Kartos.Views
                 img.AddHandler(Button.MouseDownEvent, new MouseButtonEventHandler(Image_PointMouseDown), true);
                 img.AddHandler(Button.MouseMoveEvent, new MouseEventHandler(Image_PointMouseMove), true);
                 img.AddHandler(Button.MouseUpEvent, new MouseButtonEventHandler(Image_PointMouseUp), true);
+
+                img.AddHandler(Button.MouseDownEvent, new MouseButtonEventHandler(ImageControl_MouseLeftButtonDown), true);
 
                 MainWindow.GloableConsole.Children.Add(img);
 
@@ -144,10 +148,11 @@ namespace Kartos.Views
 
         private void RayStartEnd_Click(object sender, RoutedEventArgs e)
         {
-            if(isDrawingLine == false)
+            if (isDrawingLine == false)
             {
                 isDrawingLine = true;
-            }else
+            }
+            else
                 isDrawingLine = false;
 
             _overlayCanvas = new Canvas();
@@ -197,6 +202,79 @@ namespace Kartos.Views
                 _currentLine = null;
             }
         }
+        private void CropSquare_Click(object sender, RoutedEventArgs e)
+        {
+            //Mat mat = new Mat(512,512,MatType.CV_8UC3,new Scalar(0,0,0));
+            if (isDrawingLine == false)
+            {
+                isDrawingLine = true;
+
+            }
+            else
+                isDrawingLine = false;
+            Cv2.Rectangle(mat, new OpenCvSharp.Point(50, 50), new OpenCvSharp.Point(100, 100), Scalar.Red, 2);
+
+            ShowImage(mat);
+        }
+        private void ShowImage(Mat image)
+        {
+            // Display the image
+            Cv2.ImShow("Image", image);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+
+        private List<Point> polygonPoints = new List<Point>();
+        private bool isDrawingPolygon = false;
+        private void PolygonButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isDrawingPolygon == false)
+            {
+                isDrawingPolygon = true;
+                polygonPoints.Clear();
+            }
+            else
+                isDrawingPolygon = false;
+
+            _overlayCanvas = new Canvas();
+            MainWindow.GloableConsole.Children.Add(_overlayCanvas);
+        }
+        private void ImageControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (isDrawingPolygon)
+            {
+                polygonPoints.Add(_mouseDownPosition);
+
+                if (polygonPoints.Count > 1)
+                {
+                    DrawPolygon();
+                }
+                
+            }
+        }
+
+        private void DrawPolygon()
+        {
+            DrawingVisual visual = new DrawingVisual();
+            using (DrawingContext dc = visual.RenderOpen())
+            {
+                Pen pen = new Pen(Brushes.Red, 2);
+
+                // Draw lines between each pair of consecutive points
+                for (int i = 0; i < polygonPoints.Count - 1; i++)
+                {
+                    dc.DrawLine(pen, polygonPoints[i], polygonPoints[i + 1]);
+                }
+
+                // Draw line between the last and first point to close the polygon
+                dc.DrawLine(pen, polygonPoints[polygonPoints.Count - 1], polygonPoints[0]);
+            }
+
+            // Render the visual to the image control
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)_overlayCanvas.ActualWidth, (int)_overlayCanvas.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            rtb.Render(visual);
+            _currentImage.Source = rtb;
+        }
     }
-        
-}
+} 
+
